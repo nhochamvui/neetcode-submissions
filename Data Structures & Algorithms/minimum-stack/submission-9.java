0659d7547d13586dc3cdceefcdb80a7e6217ccc9
Hoang Tho <nhochamvui.love@gmail.com>
2026-08-22 22:54:06 +0700

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> sub; 
+    public MinStack() {
+        this.stack = new ArrayDeque<Integer>();
+        this.sub = new ArrayDeque<Integer>();
+    }
+    
+    public void push(int val) {
+        this.stack.push(val);
+        if(sub.isEmpty() || val <= sub.peek()){
+            this.sub.push(val);
+        }
+    }
+    // -100, -200, -300, -400
+    public void pop() {
+        if(stack.isEmpty()) return;
+        int top = stack.peek();
+        if(!sub.isEmpty() && top == sub.peek()){
+            this.sub.pop();
+        }
+        this.stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return sub.peek();
+    }
+}
